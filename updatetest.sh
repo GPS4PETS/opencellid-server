3fa0aac7c8f60a9bf961566022aafbee9e373210
root <root@nominatim.wasn.local>
2025-03-06 15:16:20 +0000

    clean up

--- updatetest.sh
+++
@@ -1,6 +0,0 @@
-systemctl stop cell-geolocation.service
-
-cat oci_import.sql | sqlite3 oci_cells.sqlite
-cat oci_cells-cleanup.sql | sqlite3 oci_cells.sqlite
-
-systemctl start cell-geolocation.service
